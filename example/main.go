defef6bd4feaa8f1390072ef017dc4081b48f060
Panagiotis Vryonis <58812+vrypan@users.noreply.github.com>
2025-06-20 17:55:14 +0300

    Updated example, with neynar support

--- example/main.go
+++ example/main.go
@@ -9,21 +9,55 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
+// Used to add an extra HTTP header to requests.
+// Required by Neynar
+func apiKeyInterceptor(header, value string) grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req, reply any,
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		md := metadata.Pairs(header, value)
+		ctx = metadata.NewOutgoingContext(ctx, md)
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+}
+
 func main() {
-	hubAddr := "hub.merv.fun:3383" // set your hub hostname here
-	useSsl := false
+	// set your hub addr:port here
+	// port is usually 3383 for the gRPC endpoint but
+	// in case of Neynar, it's 443
+	hubAddr := "hub-grpc-api.neynar.com:443"
+
+	// Is the server using SSL?
+	useSsl := true
 	cred := insecure.NewCredentials()
 	if useSsl {
 		cred = credentials.NewClientTLSFromCert(nil, "")
 	}
+
+	// Does the server expect a special API key in headers?
+	// hubApiKey := "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+	hubApiKey := ""
+	var interceptor grpc.UnaryClientInterceptor
+	if hubApiKey != "" {
+		interceptor = apiKeyInterceptor("x-api-key", hubApiKey)
+	}
+
 	conn, err := grpc.Dial(
 		hubAddr,
 		grpc.WithTransportCredentials(cred),
+		grpc.WithUnaryInterceptor(interceptor),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(20*1024*1024)),
 	)
+
 	defer conn.Close()
 
 	if err != nil {
